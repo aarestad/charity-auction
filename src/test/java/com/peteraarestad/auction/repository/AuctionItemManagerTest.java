d70ca861f0b5bcd33c328cb9f88ae9015b08aef3
Peter Aarestad <aarestad@gmail.com>
2016-08-18 19:11:33 -0500

    more unit tests

--- src/test/java/com/peteraarestad/auction/repository/AuctionItemManagerTest.java
+++ src/test/java/com/peteraarestad/auction/repository/AuctionItemManagerTest.java
@@ -1,33 +1,62 @@
 package com.peteraarestad.auction.repository;
 
+import com.peteraarestad.auction.model.AuctionItem;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class AuctionItemManagerTest {
-    @Test
-    public void findById() throws Exception {
-        fail();
+    private AuctionItemManager auctionItemManager;
+
+    @Before
+    public void setUp() throws Exception {
+        auctionItemManager = new AuctionItemManager();
     }
 
     @Test
-    public void saveOrUpdate() throws Exception {
-        fail();
+    public void saveAndFind() throws Exception {
+        assertThat(auctionItemManager.findById(1), is(nullValue()));
+
+        AuctionItem auctionItem = new AuctionItem(1, "moo", 5);
+        auctionItemManager.saveOrUpdate(auctionItem);
+
+        assertThat(auctionItemManager.findById(1), is(auctionItem));
     }
 
     @Test
-    public void setWinningItem() throws Exception {
-        fail();
+    public void setAndGetWinningItem() throws Exception {
+        AuctionItem auctionItem1 = new AuctionItem(1, "moo", 5);
+        AuctionItem auctionItem2 = new AuctionItem(2, "boo", 4);
+
+        auctionItemManager.saveOrUpdate(auctionItem1);
+        auctionItemManager.saveOrUpdate(auctionItem2);
+
+        auctionItemManager.setWinningItem(1);
+        assertThat(auctionItemManager.getWinningItem(), is(auctionItem1));
     }
 
-    @Test
-    public void getWinningItem() throws Exception {
-        fail();
+    @Test(expected=IllegalArgumentException.class)
+    public void setWinningItem_invalidItem() throws Exception {
+        auctionItemManager.setWinningItem(1);
     }
 
     @Test
     public void currentState() throws Exception {
-        fail();
+        AuctionItem auctionItem1 = new AuctionItem(1, "moo", 5);
+        AuctionItem auctionItem2 = new AuctionItem(2, "boo", 4);
+
+        auctionItemManager.saveOrUpdate(auctionItem1);
+        auctionItemManager.saveOrUpdate(auctionItem2);
+
+        auctionItemManager.setWinningItem(1);
+
+        assertThat(auctionItemManager.currentState(), is("Items:\n" +
+                "1,moo,5,won\n" +
+                "2,boo,4,lost\n"));
     }
 
 }
--- src/test/java/com/peteraarestad/auction/repository/BillDispenserTest.java
+++ src/test/java/com/peteraarestad/auction/repository/BillDispenserTest.java
@@ -1,23 +1,61 @@
 package com.peteraarestad.auction.repository;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 public class BillDispenserTest {
-    @Test
-    public void restockInventory() throws Exception {
-        fail();
+    private BillDispenser billDispenser;
+
+    @Before
+    public void setUp() throws Exception {
+        billDispenser = new BillDispenser();
     }
 
     @Test
-    public void getPayout() throws Exception {
-        fail();
+    public void getPayout_insufficientFunds() throws Exception {
+        billDispenser.restockInventory();
+
+        assertThat(billDispenser.currentState(), is("Inventory:\n" +
+                "$1,10\n" +
+                "$5,10\n" +
+                "$10,10\n" +
+                "$20,10\n" +
+                "$100,10\n"));
+
+        assertThat(billDispenser.getPayout(5000), is(nullValue()));
+
+        assertThat(billDispenser.currentState(), is("Inventory:\n" +
+                "$1,10\n" +
+                "$5,10\n" +
+                "$10,10\n" +
+                "$20,10\n" +
+                "$100,10\n"));
     }
 
     @Test
-    public void currentState() throws Exception {
-        fail();
-    }
+    public void getPayout_sufficientFunds() throws Exception {
+        billDispenser.restockInventory();
 
+        Map<Integer, Integer> payout = billDispenser.getPayout(557);
+
+        assertThat(billDispenser.currentState(), is("Inventory:\n" +
+                "$1,8\n" +
+                "$5,9\n" +
+                "$10,9\n" +
+                "$20,8\n" +
+                "$100,5\n"));
+
+        assertThat(payout.get(1), is(2));
+        assertThat(payout.get(5), is(1));
+        assertThat(payout.get(10), is(1));
+        assertThat(payout.get(20), is(2));
+        assertThat(payout.get(100), is(5));
+    }
 }
